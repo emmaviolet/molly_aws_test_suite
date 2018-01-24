91f951cea0c4df83c0c6cd89538595972020b670
David Townsend <toonsend@gmail.com>
2018-01-24 09:15:08 +0000

    Fixed with application/json content-type in request, you need content type aswell as accept in header

---
+++ molly_connection.rb
@@ -0,0 +1,55 @@
+require 'unirest'
+
+AWS_URL = 'https://a84xihdz74.execute-api.eu-west-1.amazonaws.com/dev_toons/'
+#AWS_URL = 'http://localhost:8002/'
+
+class MollyConnection
+
+  CALL_TYPE = {
+    test_connection: ["test-api", :get],
+    create_incident: ["incident", :post]
+  }
+  HTTP_HEADERS = {
+    "Accept": "application/json",
+    "content-type": "application/json"
+  }
+
+
+  def self.test_connection
+    url_call(:test_connection)
+  end
+
+  def self.create_incident(incident_params)
+    url_call(:create_incident, { user_id: "122353" })
+  end
+
+  def self.api_key(new_key)
+    @@api_key = new_key
+  end
+
+  private
+
+  def self.url_call(rest_method, params = nil)
+    restful_method_name, http_method = CALL_TYPE[rest_method]
+    case http_method
+    when :post
+      post(restful_method_name, params)
+    when :get
+      get(restful_method_name)
+    else
+      raise Exception.new('RESTful method missing')
+    end
+  end
+
+  def self.get(rest_method)
+    Unirest.get AWS_URL + rest_method,
+      headers: HTTP_HEADERS
+  end
+
+  def self.post(rest_method, params)
+    puts AWS_URL + rest_method
+    Unirest.post AWS_URL + rest_method,
+      headers: HTTP_HEADERS
+  end
+
+end
--- spec/molly_api_spec.rb
+++ spec/molly_api_spec.rb
@@ -1,17 +1,36 @@
+require 'unirest'
+require_relative '../molly_connection'
+
 RSpec.describe MollyConnection do
+
 
   context "Incident" do
 
     let(:incident_params) { {} }
 
+    context "test connection" do
+
+      xit "should return working" do
+        expect(MollyConnection.test_connection.body).to eq({"test" => "it's working"})
+      end
+    end
+
+
     context "creation" do
 
-      it "should return an incident object" do
-        expect(MollyConnection.create_incident(incident_params)).to be_a String
-      end
+         it "should require client_id" do
+           expect(MollyConnection.create_incident({}).code).to eq(400)
+         end
+
+         xit "should return an incident object" do
+           expect(MollyConnection.create_incident(incident_params)).to be_a String
+         end
+
+         xit "should make a post request" do
+         end
+
+       end
 
     end
 
   end
-
-end
