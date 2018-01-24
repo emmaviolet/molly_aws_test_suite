99e9af8b289c8fb9146bd917139e4dbc3378af65
David Townsend <toonsend@gmail.com>
2018-01-24 11:03:43 +0000

    Requires API key to access API

--- lib/molly_connection.rb
+++ lib/molly_connection.rb
@@ -1,32 +1,44 @@
 require 'unirest'
 
-AWS_URL = 'https://a84xihdz74.execute-api.eu-west-1.amazonaws.com/dev_toons/'
 
 class MollyConnection
 
-  HTTP_HEADERS = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-  }
 
-  def self.test_connection
+  def initialize(params)
+    @aws_url = params[:api_url]
+    @api_key = params[:api_key]
+  end
+
+  def test_connection
     get_from_molly_api('test-api')
   end
 
-  def self.create_incident(incident_params)
+  def create_incident(incident_params)
     post_to_molly_api('incident', { user_id: "122353" })
+  end
+
+  def aws_url
+    @aws_url
+  end
+
+  def http_headers
+    {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "x-api-key": @api_key
+    }
   end
 
   private
 
-  def self.post_to_molly_api(rest_method, params)
-    Unirest.post AWS_URL + rest_method,
-      headers: HTTP_HEADERS
+  def post_to_molly_api(rest_method, params)
+    Unirest.post @aws_url + rest_method,
+      headers: http_headers
   end
 
-  def self.get_from_molly_api(rest_method)
-    Unirest.get AWS_URL + rest_method,
-      headers: HTTP_HEADERS
+  def get_from_molly_api(rest_method)
+    Unirest.get @aws_url + rest_method,
+      headers: http_headers
   end
 
 end
--- spec/lib/molly_api_spec.rb
+++ spec/lib/molly_api_spec.rb
@@ -7,28 +7,57 @@ RSpec.describe MollyConnection do
   context "Incident" do
 
     let(:incident_params) { {} }
+    let(:http_headers) {
+      {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    }
 
     context "test connection" do
 
       it "should return working" do
-        expect(MollyConnection.test_connection.body).to eq({"test" => "it's working"})
+        mc = new_molly_connection
+        expect(mc.test_connection.body).to eq({"test" => "it's working"})
       end
+
+      it "should require an API key" do
+        mc = new_molly_connection({api_key: ''})
+        response = mc.test_connection
+        expect(response.code).to eq(403)
+        expect(response.body).to eq({
+          "message" => "Forbidden"
+        })
+      end
+
     end
 
 
-    context "creation" do
+    context "succesful incident creation" do
 
       it "should return a 200 response" do
-        expect(MollyConnection.create_incident({}).code).to eq(200)
+        mc = new_molly_connection
+        response = mc.create_incident(incident_params)
+        expect(response.code).to eq(200)
       end
 
-      it "should return an incident object" do
-        incident = MollyConnection.create_incident(incident_params).body
+      it "should return a incident guid" do
+        mc = new_molly_connection
+        incident = mc.create_incident(incident_params).body
         expect(incident['incident_guid']).to be_a String
       end
 
-      it "should require client_id"
-      it "should make a post request"
     end
+
+    context "creation fails" do
+      it "without a client_id"
+    end
+  end
+
+  def new_molly_connection(params = {})
+    MollyConnection.new({
+      api_url: 'https://a84xihdz74.execute-api.eu-west-1.amazonaws.com/dev_toons/',
+      api_key: '4vhGo3h1Kd8cSNONCCCge1jXLIHuE53p8t7Hl9JG'
+    }.merge(params))
   end
 end
