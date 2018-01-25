724a044d568f61a31f9085fdac89b8fd6d5535bf
David Townsend <toonsend@gmail.com>
2018-01-25 16:59:34 +0000

    Added readme

--- README.txt
+++ README.txt
@@ -1,1 +1,13 @@
 This is a test suite for Molly AWS Gateway API
+
+You need to set up env file on your local machine for the specs to work
+
+** create a file named ~/app-env with the following entries
+
+export MOLLY_AWS_TEST_API_KEY=<AWS TEST API KEY>
+export MOLLY_AWS_TEST_URL=<AWS API TEST URL>
+
+** add the following line to your .bash_profile
+
+source ~/app-env
+
--- spec/lib/molly_api_spec.rb
+++ spec/lib/molly_api_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe MollyConnection do
   context "Incident" do
 
     let(:incident_params) { {
-      client_id: 'some-client-co-uk',
+      client_id: 'this-is-pain-some-client-co-uk',
       app_id: "test-server",
       reporter_email: "reporter@molly.com",
       reporter_name: "Molly Bulregard Server",
@@ -24,12 +24,12 @@ RSpec.describe MollyConnection do
 
     context "test connection" do
 
-      it "should return working" do
+      xit "should return working" do
         mc = new_molly_connection
         expect(mc.test_connection.body).to eq({"test" => "it's working"})
       end
 
-      it "should require an API key" do
+      xit "should require an API key" do
         mc = new_molly_connection({api_key: ''})
         response = mc.test_connection
         expect(response.code).to eq(403)
@@ -43,16 +43,33 @@ RSpec.describe MollyConnection do
 
     context "succesful incident creation" do
 
-      it "should return a 200 response" do
+      xit "should require an API key" do
+        mc = new_molly_connection({api_key: ''})
+        response = mc.create_incident(incident_params)
+        expect(response.code).to eq(403)
+        expect(response.body).to eq({
+          "message" => "Forbidden"
+        })
+      end
+
+      xit "should return a 200 response" do
         mc = new_molly_connection
         response = mc.create_incident(incident_params)
         expect(response.code).to eq(200)
       end
 
-      it "should return a incident guid" do
+      xit "should return a incident id" do
+        mc = new_molly_connection
+        puts incident_params
+        incident = mc.create_incident(incident_params).body
+        expect(incident['incident_id']).to be_a String
+      end
+
+      xit "should return incident object" do
         mc = new_molly_connection
         incident = mc.create_incident(incident_params).body
-        expect(incident['incident_guid']).to be_a String
+        incident.delete('incident_id')
+        expect(incident.keys).to eq(incident_params.keys.collect {|k| k.to_s })
       end
 
     end
@@ -66,7 +83,7 @@ RSpec.describe MollyConnection do
         :incident_type,
         :incident_description,
         :named_party_name ].each do |required_param|
-          it "without a #{required_param}" do
+          xit "without a #{required_param}" do
             mc = new_molly_connection
             incident_without_required_param = incident_params
             incident_without_required_param.delete(required_param)
@@ -74,14 +91,21 @@ RSpec.describe MollyConnection do
             expect(response.code).to eq 400
           end
       end
+
+      it "should not accept unspecified values" do
+        puts "HHHHHH #{ENV['MOLLY_AWS_TEST_API_KEY']} HHHH"
+        mc = new_molly_connection
+        incident_with_extra_params = incident_params
+        incident_with_extra_params['bad_key'] = "I'm trying to add a value I'm not supposed to"
+        response = mc.create_incident(incident_with_extra_params)
+        expect(response.code).to eq 400
+      end
     end
   end
 
   def new_molly_connection(params = {})
     MollyConnection.new({
-      api_url: 'https://a84xihdz74.execute-api.eu-west-1.amazonaws.com/dev_toons/',
- #     api_url: 'localhost:8000/',
-      api_key: '4vhGo3h1Kd8cSNONCCCge1jXLIHuE53p8t7Hl9JG'
+      api_url: ENV['MOLLY_AWS_TEST_URL'],
+      api_key: ENV['MOLLY_AWS_TEST_API_KEY']
     }.merge(params))
-  end
-end
+  end end
